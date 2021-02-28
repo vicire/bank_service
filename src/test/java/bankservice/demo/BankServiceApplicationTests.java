a560e70fa6d5ff33eb672caf0a6848ae0a62be0d
Roman Saviak <saviak.roman@gmail.com>
2021-02-28 19:51:38 +0200

    first commit

---
+++ src/test/java/bankservice/demo/BankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bankservice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
